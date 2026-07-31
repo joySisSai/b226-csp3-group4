fc3df6983f7df9d8a2bd926427c88405d5a079e1
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-07-31 12:28:11 +0800

    feat: add database connection factory

---
+++ src/main/java/com/joysistvi/brgyconnectapp/config/DbConnection.java
@@ -0,0 +1,20 @@
+package com.joysistvi.brgyconnectapp.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection implements ConnectionFactory {
+    private final static String URL = "jdbc:mysql://localhost:3306/barangayconnect_db";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "";
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    @Override
+    public Connection openConnection() throws SQLException {
+        return connect();
+    }
+}
