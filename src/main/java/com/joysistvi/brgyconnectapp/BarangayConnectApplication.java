0254c800a8fd36c1a7226d49c8075729dabdfd39
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-07-31 12:29:44 +0800

    feat: wire application components

---
+++ src/main/java/com/joysistvi/brgyconnectapp/BarangayConnectApplication.java
@@ -0,0 +1,44 @@
+package com.joysistvi.brgyconnectapp;
+
+import com.joysistvi.brgyconnectapp.config.ConnectionFactory;
+import com.joysistvi.brgyconnectapp.config.DbConnection;
+import com.joysistvi.brgyconnectapp.controller.AuthController;
+import com.joysistvi.brgyconnectapp.model.User;
+import com.joysistvi.brgyconnectapp.repository.UserRepo;
+import com.joysistvi.brgyconnectapp.repository.UserRepoImpl;
+import com.joysistvi.brgyconnectapp.service.AuthService;
+import com.joysistvi.brgyconnectapp.view.AdminDashboard;
+import com.joysistvi.brgyconnectapp.view.DashboardRouter;
+import com.joysistvi.brgyconnectapp.view.LoginView;
+import com.joysistvi.brgyconnectapp.view.ResidentDashboard;
+import com.joysistvi.brgyconnectapp.view.StaffDashboard;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class BarangayConnectApplication {
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new DbConnection();
+        UserRepo userRepo = new UserRepoImpl(connectionFactory);
+        AuthService authService = new AuthService(userRepo);
+        AuthController authController = new AuthController(authService);
+
+        Scanner scanner = new Scanner(System.in);
+        LoginView loginView = new LoginView(authController, scanner);
+        DashboardRouter dashboardRouter = new DashboardRouter(
+                new AdminDashboard(scanner),
+                new StaffDashboard(scanner),
+                new ResidentDashboard(scanner)
+        );
+
+        while (true) {
+            Optional<User> authenticatedUser = loginView.show();
+            if (authenticatedUser.isEmpty()) {
+                System.out.println("Goodbye.");
+                break;
+            }
+
+            dashboardRouter.showDashboard(authenticatedUser.get());
+        }
+    }
+}
